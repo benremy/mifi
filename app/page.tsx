b8794774ce50c802f120362c8516688e22d13a06
WhatWasHisNameAgain <45083673+whatwashisnameagain@users.noreply.github.com>
2024-08-17 09:56:00 +0000

    add atomic component placeholders

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <p>Mifi App</p>
+      <div>
+        <p>this is the main container</p>
+        <p>navbar</p>
+        <ul>
+          <li>ledger</li>
+          <li>buckets</li>
+          <li>settings</li>
+        </ul>
+      </div>
+      <div>
+        <p>08/16/24</p>
+        <p>segmented control</p>
+        <p>amount input</p>
+        <p>text area description</p>
+        <p>add button</p>
+      </div>
+    </main>
+  );
+}
