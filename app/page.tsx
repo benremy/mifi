5b120d85fd7bfb4edee024d2bed3f7109ef99a48
WhatWasHisNameAgain <45083673+whatwashisnameagain@users.noreply.github.com>
2024-08-17 10:35:40 +0000

    shop shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,52 @@
-import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useState } from 'react'
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+
+
 
 export default function Home() {
+
+  const [date, setDate] = useState<Date | undefined>(new Date())
+
   return (
     <main>
       <p>Mifi App</p>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <Badge variant="outline">Badge</Badge>
+      <Button variant="outline">Button</Button>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
       <div>
         <p>this is the main container</p>
         <p>navbar</p>
