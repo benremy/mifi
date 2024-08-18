f4069457e49629c7e1423477473fb84b772dba1f
WhatWasHisNameAgain <45083673+whatwashisnameagain@users.noreply.github.com>
2024-08-18 20:19:33 +0000

    integrate supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 "use client"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Transaction } from '@/types'
 import AddTransaction from "@/components/add-transaction-widget"
 import Ledger from "@/components/ledger-widget"
 import Bucket from "@/components/bucket-widget"
 import Settings from "@/components/settings-widget"
+import { createClient } from '@/utils/supabase/client'
+import { cookies } from 'next/headers'
 
 export default function Home() {
+
   const [transactions, setTransactions] = useState<Transaction[]>([])
+  // const cookieStore = cookies()
+  const supabase = createClient()
+
+  const testTodos = async () => {
+    const { data: todos } = await supabase.from('transactions').select()
+    console.log('We have todos', todos)
+  }
+
+  useEffect(() => {
+    testTodos()
+    
+  },[])
+
+
   return (
     <main className="w-screen flex overflow-hidden">
       <AddTransaction 
